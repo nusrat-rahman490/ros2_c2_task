9aa988fd07824e4c16d43c574ae2bc9846651f79
nusrat-rahman490 <rahmannusrat490@gmail.com>
2025-07-13 17:21:20 +0600

    Create arduino_code.ino

---
+++ arduino/arduino_code.ino
@@ -0,0 +1,35 @@
+#define LED 13
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  
+  int valA = random(-100, 101);
+  float valB = random(-100, 101) / 100.0;
+  int valC = random(0, 1024);
+  String msg = "Hi from UBUNTU";
+
+  Serial.print(valA);
+  Serial.print(",");
+  Serial.print(valB, 3);
+  Serial.print(",");
+  Serial.print(valC);
+  Serial.print(",");
+  Serial.println(msg);
+
+  delay(2000);
+
+  
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    if (command == "OFF") {
+      digitalWrite(LED , LOW);
+    } else if (command == "ON") {
+      digitalWrite(LED, HIGH);
+    }
+  }
+}
